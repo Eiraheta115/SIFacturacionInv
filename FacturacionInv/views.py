f01cf4eaca355856030bb973b135a6d3623c2cd5
felix carpio <felixcarpio9312@gmail.com>
2019-01-17 16:33:19 -0600

    push

--- FacturacionInv/views.py
+++ FacturacionInv/views.py
@@ -7,6 +7,8 @@ from .forms import ProveedoresForm
 from .models import Clientes
 from django.forms import ModelForm
 from django.views.generic import ListView
+from django.http import HttpResponse
+import json
 
 
 # Create your views here.
@@ -25,12 +27,25 @@ def buscar(listClientes, busqueda):
 def nueva_venta(request):
 	template = "../templates/nueva_venta.html"
 
-	clientes2 = list()
-	cliente = Clientes.objects.all()
+	search_term = ''
+	search_term_1 = ''
+	productos = ''
+	cliente = ''
 
-	for cl in cliente:
-		clientes2.append(cl.nombre)
+#	clientes2 = list()
+#	cliente = Clientes.objects.all()
+#
+#	for cl in cliente:
+#		clientes2.append(cl.nombre)
 
+	#productos = Productos.objects.all()
+	if 'search' in request.GET:
+		search_term = request.GET['search']
+		productos = Productos.objects.filter(nombre__icontains=search_term)
+
+	if 'search_client' in request.GET:
+		search_term = request.GET['search_client']
+		cliente = Clientes.objects.filter(nombre__icontains=search_term_1)
 
 	if request.method == 'POST':
 		fecha = request.POST.get("fecha")
@@ -53,27 +68,30 @@ def nueva_venta(request):
 		else:
 			idfactncred = null
 			# idtdoc = 2
+
+		detallefactura = Detallefacturas(cantidad=cantidad, precio=precio, subtotal=subtotal, descuento=descuento,
+			idfactura=idfactura, idproducto=idproducto, idbodega=idbodega)
+
+		'''
 		fatura = Facturas(codigo=codigo, fecha=fecha, concepto=concepto, vencimiento=vencimiento, 
 				total=total, exenta=exenta, descuento=descuento, anulado=anulado, impreso=impreso, 
 				idtdoc=idtdoc, idcliente=idcliente, idfactncred=idfactncred)
 		factura.save()
 
+		movimiento = Movimiento(fecha=fecha, costomovimiento=costomovimiento, costopromedio=costopromedio, 
+			concepto=concepto, responsable=responsable, cantidad=cantidad, bodega1=bodega1, bodega2=bodega2,
+			codproducto=codproducto, tmov=tmov, idfactura=idfactura, idtdoctransferencia=idtdoctransferencia,
+			idlibcom=idlibcom)
+		movimiento.save()
 
+		producto = Producto(codigoprod=codigoprod, nombre=nombre, descripcion=descripcion, marca=marca, 
+			existenciamax=existenciamax, existenciamin=existenciamin, existenciaactual=existenciaactual,
+			idcategoria=idcategoria, idbodega=idbodega)
+		producto.save()
+		'''
 
-	'''
-
-	movimiento = Movimientos(fecha=fecha, costomovimiento=costomovimiento, costopromedio=costopromedio,
-		concepto=concepto, responsable=responsable, cantidad=cantidad, bodega1=bodega1, bodega2=bodega2, 
-		codproducto=codproducto, tmov=tmov, idfactura=idfactura, idtdoctransferencia=idtdoctransferencia, 
-		idlibcom=idlibcom)
-	movimiento.save()
-
-	producto = Prodcutos(codigoprod=codigoprod, nombre=nombre, descripcion=descripcionproducto, marca=marcaproducto, 
-		existenciamax=existenciamax, existenciamin=existenciamin, idcategoria=idcategoria, idbodega=idbodega)
-	producto.save()
-	'''
 	context = {
-			# 'codigoprod':codigoprod, 'descripcionproducto':descripcion
+		'search_term':search_term, 'productos':productos, 'cliente': cliente,
 	}
 
 	return render(request, template, context)
@@ -105,9 +123,6 @@ def prueba(request):
 		search = cprod.lower()
 		cod = buscar_prod(products, search)
 		print (cod)
-		new = list()
-		new = listarproductos(products, search)
-	context = {'new':new}
 	'''
 	if request.method == 'POST':
 		buscar = request.POST.get("codprod")
@@ -125,7 +140,7 @@ def prueba(request):
 		print(idprodname)
 	'''
 
-	return render(request, template, context)
+	return render(request, template)
 
 def nueva_compra(request):
 	return render(request, "../templates/nueva_compra.html")
@@ -269,3 +284,6 @@ class ProductosList(ListView):
     model = Productos
     template_name = "../templates/productlist.html"
 
+def busqueda(request): 
+  usuario = {'nombre': 'Eduardo Ismael'} 
+  return HttpResponse( json.dumps(usuario), content_type='application/json' )
